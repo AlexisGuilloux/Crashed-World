982a283bbf0dcde65799e972219dcef470863b47
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2022-12-13 14:46:00 +0100

    Add interface for upgradable and usable items

---
+++ Assets/Scripts/Item/Interface/IUpgradable.cs
@@ -0,0 +1,17 @@
+using CrashedWorld.Crafts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrashedWorld.Items
+{
+	public interface IUpgradable
+	{
+		/// <summary>
+		/// The function return null if the object can no longer be upgraded
+		/// </summary>
+		/// <returns>The upgrade recipe</returns>
+		Recipe GetUpgradeRecipe();
+	}
+}
+	
--- Assets/Scripts/Item/UsableItem/HealthPotion.cs
+++ Assets/Scripts/Item/UsableItem/HealthPotion.cs
@@ -1,16 +1,23 @@
+using CrashedWorld.Crafts;
 using UnityEngine;
 
 namespace CrashedWorld.Items
 {
 	[CreateAssetMenu(fileName = "New Potion", menuName = "Crashed World/Item/Health Potion")]
-	public class HealthPotion : Item
+	public class HealthPotion : Item, IUsable, IUpgradable
     {
 		[Space, Header("   Potion")]
         public int health;
+		public Recipe upgradeRecipe;
 
-		public override void Use()
+		public void Use()
 		{
 			// Give player 'health' amount of health.
+		}
+
+		public Recipe GetUpgradeRecipe()
+		{
+			return upgradeRecipe;
 		}
 	}
 }
