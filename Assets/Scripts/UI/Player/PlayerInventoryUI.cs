f90a6190f5ede19cc1dd6f7e7c22bb258ec055e8
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2022-12-14 17:56:45 +0100

    Add better hide and show handling to ui

--- Assets/Scripts/UI/Player/PlayerInventoryUI.cs
+++ Assets/Scripts/UI/Player/PlayerInventoryUI.cs
@@ -11,15 +11,21 @@ namespace CrashedWorld.UI
 	//TODO - Handle inventory full (but not in ui)
 	public class PlayerInventoryUI : MonoBehaviour
 	{
+		[Header("   Display")]
+		public CanvasGroup canvas;
+
 		[Header("   References")]
 		public GridLayoutGroup grid;
 		public Image dragImage;
 		public RecipeListUI recipeList;
 
+		public bool IsOpen => canvas.alpha > 0;
+
 		private List<GridSlotUI> slots = new List<GridSlotUI>();
 
 		void Awake()
 		{
+			Hide();
 			Locator.playerInventory = this;
 		}
 
@@ -33,6 +39,30 @@ namespace CrashedWorld.UI
 		private void OnDestroy()
 		{
 			PlayerInventory.Instance.bag.OnAddItem -= Bag_OnAddItem;
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.B))
+				Toggle(!IsOpen);
+		}
+
+		public void Toggle(bool isON)
+		{
+			if (isON)
+				Show();
+			else
+				Hide();
+		}
+
+		public void Show()
+		{
+			canvas.alpha = 1;
+		}
+
+		public void Hide()
+		{
+			canvas.alpha = 0;
 		}
 
 		private void Bag_OnAddItem(string itemID, int value)
