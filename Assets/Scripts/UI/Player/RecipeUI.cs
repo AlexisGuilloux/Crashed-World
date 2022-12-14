dcba730ed1d9e5fcb7fa7f312de436e1199fe957
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2022-12-14 17:48:39 +0100

    Craft system work with pin and shortcut

--- Assets/Scripts/UI/Player/RecipeUI.cs
+++ Assets/Scripts/UI/Player/RecipeUI.cs
@@ -3,16 +3,21 @@ using CrashedWorld.Managers;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CrashedWorld.UI
 {
-	public class RecipeUI : MonoBehaviour
+	public class RecipeUI : MonoBehaviour, IPointerClickHandler
     {
         public TextMeshProUGUI label;
         public List<RecipeRequirementUI> requirements;
 
+		private Recipe recipe;
+
         public void Init(Recipe recipe)
 		{
+			this.recipe = recipe;
+
             label.text = ItemManager.Instance.database.Get(recipe.result).itemName;
             requirements.ForEach(r => r.Hide());
             for(int i = 0; i < recipe.recipies.Count; i++)
@@ -21,7 +26,13 @@ namespace CrashedWorld.UI
                 requirements[i].Show();
 			}
 		}
-    }
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			CraftManager.Instance.SelectRecipe(recipe);
+			Locator.playerInventory.recipeList.Hide();
+		}
+	}
 }
 
 
